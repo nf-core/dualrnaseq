08f84681ff97dd36fd003c0b615d6adecc4bc10f
bozmik <bozena.mika1@gmail.com>
2020-05-12 12:10:08 +0200

    deal with .fq in sample names - count_total_reads.sh

--- bin/count_total_reads.sh
+++ bin/count_total_reads.sh
@@ -4,8 +4,11 @@ for i in "$@"
 do
 lines=$(zcat -f $i| wc -l)
 count=$(($lines / 4))
-
 name=$(echo "$i" | tr \: _)
-echo -n -e "$name\t$count\n"
+name2=$(echo "${name/.fastq.gz/}")
+name2=$(echo "${name2/.fq.gz/}")
+name2=$(echo "${name2/.fastq/}")
+name2=$(echo "${name2/.fq/}")
+echo -n -e "$name2\t$count\n"
 done
 
