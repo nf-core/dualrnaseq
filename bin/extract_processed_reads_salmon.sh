571a2000dbbaef15d3101a81024b4a0c65afc84c
bozmik <bozena.mika1@gmail.com>
2020-05-25 13:45:49 +0200

    extract processed reads from star

--- bin/extract_processed_reads_salmon.sh
+++ bin/extract_processed_reads_salmon.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-processed=$(grep "num_processed" $1 | sed 's/num_processed//g'| sed 's/[^a-zA-Z0-9]//g') 
+if [ $3 = "salmon" ]; then
+	processed=$(grep "num_processed" $1 | sed 's/num_processed//g'| sed 's/[^a-zA-Z0-9]//g') 
+	echo -e "$2\t${processed}" > $2.txt
+elif [ $3 == "star" ]; then
+	processed=$(grep "Number of input reads" $1 | sed 's/Number of input reads//g'| sed 's/[^a-zA-Z0-9]//g')
+	echo -e "$2\t${processed}" > $2.txt
+fi
 
-echo -e "$2\t${processed}" > $2.txt
+
+
 
