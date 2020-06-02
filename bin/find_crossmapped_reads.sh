134311a9b6749c3fbf60f5ec063c4f49f8dca922
Bozena Mika-Gospodorz <bozena.mika-gospodorz@helmholtz-hiri.de>
2020-06-02 11:16:29 +0200

    fix find_crossmapped_reads.sh

--- bin/find_crossmapped_reads.sh
+++ bin/find_crossmapped_reads.sh
@@ -7,4 +7,4 @@ pathogen_reference=$4
 out_name=$5
 
 
-samtools view -F 4 -h $alignment | fgrep -vw NH:i:1 | $extract_crossmapped_reads_path/extract_crossmapped_reads.py -h_ref $host_reference -p_ref $pathogen_reference -o $out_name
+samtools view -F 4 -h $alignment | fgrep -vw NH:i:1 | python $extract_crossmapped_reads_script_path/extract_crossmapped_reads.py -h_ref $host_reference -p_ref $pathogen_reference -o $out_name
