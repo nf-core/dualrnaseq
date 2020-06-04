887310347bd0c69d4ead7c4335b137aa09782490
bozmik <bozena.mika1@gmail.com>
2020-06-04 15:15:29 +0200

    try to fix salmon tximport

--- bin/split_quant_tables_salmon.sh
+++ bin/split_quant_tables_salmon.sh
@@ -5,9 +5,9 @@ quant_table=$3
 suffix=$4
 
 grep ">" $transcriptome_pathogen | awk -F ">" '{ print $2 }' | awk 'NR==FNR{a[$0]=$0}NR>FNR{if($1==a[$1])print $0}' - $quant_table > pathogen_quant
-awk 'NR==1 {print; exit}' $quant_table | cat - pathogen_quant > pathogen_quant_$suffix
+awk 'NR==1 {print; exit}' $quant_table | cat - pathogen_quant > pathogen_quant$suffix
 
 grep ">" $transcriptome_host | awk -F ">" '{ print $2 }' | awk -F ' ' '{print $1}' | awk 'NR==FNR{a[$0]=$0}NR>FNR{if($1==a[$1])print $0}' - $quant_table > host_quant
-awk 'NR==1 {print; exit}' $quant_table | cat - host_quant > host_quant_$suffix
+awk 'NR==1 {print; exit}' $quant_table | cat - host_quant > host_quant$suffix
 
 
