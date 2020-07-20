7ad438e1ee11ddaf88befe308af72a723d27ffe8
Bozena Mika-Gospodorz <bozena.mika-gospodorz@helmholtz-hiri.de>
2020-07-20 14:56:18 +0200

    fix cross_mapped reads- paired-end

--- bin/count_cross_mapped_read_pairs.sh
+++ bin/count_cross_mapped_read_pairs.sh
@@ -3,9 +3,8 @@
 for i in "$@"
 do
 lines=$(wc -l $i | awk '{print $1}')
-count_reads=$(($lines / 2))
 
-echo -n -e "$i\t$count_reads\n" >> cross_mapped_reads_sum.txt
+echo -n -e "$i\t$lines\n" >> cross_mapped_reads_sum.txt
 done
 
 
