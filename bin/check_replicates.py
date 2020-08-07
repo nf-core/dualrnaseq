e010d7529054eb3ed34e739ed78c97475e46756d
reganhayward <reganhayward@gmail.com>
2020-08-07 11:46:40 +1000

    Update check_replicates.py
    Added in comments
    Updated the print statement to correctly capture R1 and R2 - and r1 and r2

--- bin/check_replicates.py
+++ bin/check_replicates.py
@@ -11,8 +11,11 @@ parser = argparse.ArgumentParser(description="""Are there replicates?""")
 parser.add_argument("-s", "--sample_names", metavar='<input_files>', nargs='+')
 args = parser.parse_args()
 
-sample_names = [feature.replace('[' , '').replace(']','').replace(',','') for feature in args.sample_names]
-suffixes = [name.rsplit('_', 1)[1] for name in sample_names]
+sample_names = [feature.replace('[' , '').replace(']','').replace(',','') for feature in args.sample_names] #format input file names
+suffix_read = [name.rsplit('_', 1)[1] for name in sample_names] #Captures r1, r2
+suffix_rep = [name.rsplit('_', 2)[1] for name in sample_names] #captures R1, R2
 
-print("R1" in suffixes and "R2" in suffixes or "r1" in suffixes and "r2" in suffixes)
+#Prints either True or False
+#Looking for R1 and R2, or r1 and r2
+print("R1" in suffix_rep and "R2" in suffix_rep or "r1" in suffix_read and "r2" in suffix_read)
 
