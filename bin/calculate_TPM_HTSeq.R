9af9708b9d5e7e6041749faa90abf61786b591df
bozmik <bozena.mika1@gmail.com>
2020-08-24 18:22:28 +0200

    fix  bin/calculate_TPM_HTSeq.R

--- bin/calculate_TPM_HTSeq.R
+++ bin/calculate_TPM_HTSeq.R
@@ -6,12 +6,23 @@ library(rtracklayer)
 args = commandArgs(trailingOnly=TRUE)
 gene_attribute <- args[2]
 table_htseq <- read.table(args[1], sep="\t", stringsAsFactors=F, header = T,row.names = gene_attribute) 
-
 pathogen_gff <- import(args[3])
 
 host_gff <- import(args[4])
 pathogen_gff <- import(args[3])
-  
+
+
+extract_gene_length <- function(x,host_gff){
+  #find annotations that contain a host gene_id (hosT_gene)
+  h_gene <- host_gff[host_gff$gene_id == x]
+  # Filter quant features
+  quants <- h_gene[h_gene$type == "quant",]
+  ## reduce - merge overlapping features, so that the same bases are covered by a reduced collection of features.
+  g_length <- sum(width(reduce(quants)))
+  return(g_length)
+}
+
+
 if(gene_attribute == 'gene_id'){
   #find pathogen genes
   common_pathogen = intersect(rownames(table_htseq),pathogen_gff$gene_id)
@@ -23,20 +34,12 @@ if(gene_attribute == 'gene_id'){
   common_host = intersect(rownames(table_htseq),host_gff$gene_id)
   #extract quantification results of host genes
   host_table_htseq <- table_htseq[common_host,]
-  #find index of host genes in host_gff object
-  for(hosT_gene in common_host){
-    #find annotations that contain a host gene_id (hosT_gene)
-    inx_genes = which(host_gff$gene_id == hosT_gene)
-    h_gene <- host_gff[inx_genes]
-    # Filter quant features
-    quants <- h_gene[h_gene$type == "quant",]
-    ## reduce - merge overlapping features, so that the same bases are covered by a reduced collection of features.
-    g_length <- sum(width(reduce(quants)))
-    #add 'gene length' to the quantification table 
-    host_table_htseq <- cbind(length = g_length,host_table_htseq)
-  }
-  
+  #extract gene length
+  gene_length <- sapply(common_host, extract_gene_length, host_gff=host_gff,simplify = TRUE)
+  host_table_htseq <- cbind(length = gene_length,host_table_htseq)
 }
+
+
 
 # splitting up isoforms as preparation for the next step
 #tmp <- split(quants,as.character(quants$Parent))
