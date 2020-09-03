4ed396eb5c1452440d463d41ac1e634ae3edbe91
Bozena Mika-Gospodorz <bozena.mika-gospodorz@helmholtz-hiri.de>
2020-09-03 11:08:54 +0200

    made calculate_TPM_HTSeq.R independent on gene_attribute

--- bin/calculate_TPM_HTSeq.R
+++ bin/calculate_TPM_HTSeq.R
@@ -10,20 +10,14 @@ pathogen_gff <- import(args[3])
 
 host_gff <- import(args[4])
 pathogen_gff <- import(args[3])
-  
-extract_gene_length <- function(x,host_gff){
-  #find annotations that contain a host gene_id (hosT_gene)
-  h_gene <- host_gff[host_gff$gene_id == x]
-  # Filter quant features
-  quants <- h_gene[h_gene$type == "quant",]
-  ## reduce - merge overlapping features, so that the same bases are covered by a reduced collection of features.
-  g_length <- sum(width(reduce(quants)))
-  return(g_length)
-}
 
-extract_transcript_length <- function(x,host_gff){
-  #find annotations that contain a host gene_id (hosT_gene)
-  h_tr <- host_gff[host_gff$transcript_id == x]
+#pathogen_gff <- import('~/Desktop/PhD_studies/Dual_RNA_seq/results/results_Hela_Salmonella_simulations_SingleEnd_75_ID/references/Salmonella_combined_BMG3_quant_feature_new_attribute.gff3')
+#table_htseq <- read.table('~/Desktop/PhD_studies/Dual_RNA_seq/results/results_Hela_Salmonella_simulations_SingleEnd_75_ID/HTSeq/uniquely_mapped/quantification_results_uniquely_mapped.csv', sep="\t", stringsAsFactors=F, header = T,row.names = 'ID')
+#host_gff <- import('~/Desktop/PhD_studies/Dual_RNA_seq/results/results_Hela_Salmonella_simulations_SingleEnd_75_ID/references/gencode.v33.chr_patch_hapl_scaff.annotation_with_tRNA_quant_feature.gff3')
+
+extract_transcript_length <- function(x,host_gff,gene_attribute){
+  #find annotations that contain host transcripts
+  h_tr <- host_gff[mcols(host_gff)[gene_attribute][[1]]==x]
   # Filter quant features
   quants <- h_tr[h_tr$type == "quant",]
   ## reduce - merge overlapping features, so that the same bases are covered by a reduced collection of features.
@@ -32,39 +26,34 @@ extract_transcript_length <- function(x,host_gff){
 }
 
 
-if(gene_attribute == 'gene_id'){
-  #find pathogen genes
-  common_pathogen = intersect(rownames(table_htseq),pathogen_gff$gene_id)
-  pathogen_table_htseq <- table_htseq[common_pathogen,]
-  pathogen_gff_match <- match(common_pathogen,pathogen_gff$gene_id)
-  pathogen_table_htseq <- cbind(length = pathogen_gff@ranges@width[pathogen_gff_match],pathogen_table_htseq) 
-  
-  #find host genes
-  common_host = intersect(rownames(table_htseq),host_gff$gene_id)
-  #extract quantification results of host genes
-  host_table_htseq <- table_htseq[common_host,]
-  #extract gene length
-  gene_length <- sapply(common_host, extract_gene_length, host_gff=host_gff,simplify = TRUE)
-  host_table_htseq <- cbind(length = gene_length,host_table_htseq)
-}else if(gene_attribute == 'transcript_id'){
-  common_pathogen = intersect(rownames(table_htseq),pathogen_gff$transcript_id)
-  pathogen_table_htseq <- table_htseq[common_pathogen,]
-  pathogen_gff_match <- match(common_pathogen,pathogen_gff$transcript_id)
-  pathogen_table_htseq <- cbind(length = pathogen_gff@ranges@width[pathogen_gff_match],pathogen_table_htseq) 
-  
-  #remove positions without transcript_id (eg. genes)
-  lack_of_trnascrip_id <- which(is.na(host_gff$transcript_id))
-  host_gff <- host_gff[-lack_of_trnascrip_id]
-  #find host transcripts
-  common_host = intersect(rownames(table_htseq),host_gff$transcript_id)
-  #extract quantification results of host genes
-  host_table_htseq <- table_htseq[common_host,]
 
-  #extract gene length
-  transcript_length <- sapply(common_host, extract_transcript_length, host_gff=host_gff,simplify = TRUE)
-  host_table_htseq <- cbind(length = transcript_length,host_table_htseq)
+#pathogen
+#find pathogen genes
+names_gff_pathogen <- mcols(pathogen_gff)[gene_attribute][[1]]
+common_pathogen = intersect(rownames(table_htseq), names_gff_pathogen)
+# `$`(pathogen_gff , 'ID')
+pathogen_table_htseq <- table_htseq[common_pathogen,]
+pathogen_gff_match <- match(common_pathogen,names_gff_pathogen)
+pathogen_table_htseq <- cbind(length = pathogen_gff@ranges@width[pathogen_gff_match],pathogen_table_htseq) 
   
+#host
+#remove positions without the gene_attribute (eg.genes that don't have transcript_id attribute)
+names_gff_host <- mcols(host_gff)[gene_attribute][[1]]
+lack_of_attribute <- which(is.na(names_gff_host))
+if (length(lack_of_attribute)> 0) {
+  host_gff <- host_gff[-lack_of_attribute]
+  names_gff_host <- names_gff_host[-lack_of_attribute]
 }
+
+#find host transcripts
+common_host = intersect(rownames(table_htseq),names_gff_host)
+#extract quantification results of host genes
+host_table_htseq <- table_htseq[common_host,]
+
+#extract gene length
+transcript_length <- sapply(common_host, extract_transcript_length, host_gff=host_gff,gene_attribute = gene_attribute,simplify = TRUE)
+host_table_htseq <- cbind(length = transcript_length,host_table_htseq)
+
 
 
 
