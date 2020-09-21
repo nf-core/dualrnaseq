9b2726dddc232e4cccadb0744d057425c9b37b09
Bozena Mika-Gospodorz <bozena.mika-gospodorz@helmholtz-hiri.de>
2020-09-21 16:48:35 +0200

    add code descriptions

--- bin/replace_pathogen_attribute_gff.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-input_gff=$1
-out_file=$2
-
-awk -v pathogen="$4" -v host="$3" -F '\t' '{gsub(pathogen,host,$9);print $1"\t"$2"\t"$3"\t"$4"\t"$5"\t"$6"\t"$7"\t"$8"\t"$9}' "${input_gff}" > ${out_file}
-
-
-# OFS='\t'
