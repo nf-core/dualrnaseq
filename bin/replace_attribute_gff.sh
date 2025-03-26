411280c8898d78ebf93fb92c9132a76f02a4f4ce
jen-reeve <31419037+jen-reeve@users.noreply.github.com>
2025-03-26 02:37:24 +0000

    bin linting

--- bin/replace_attribute_gff.sh
+++ bin/replace_attribute_gff.sh
@@ -2,7 +2,7 @@
 
 #-------------------------
 #
-# Description: 
+# Description:
 # Script to replace gene attributes in 9th column of gff file, e.g. locus_tag with transcript_id, transcript_id with parent
 #
 # Created by B. Mika-Gospodorz
@@ -11,7 +11,7 @@
 # Input files:  $1 gff file
 # 		$2 output file name
 # 		$3 new attribute
-# 		$4 current attribute 
+# 		$4 current attribute
 # Output: gff file defined in the 2nd argument
 #
 #-------------------------
